ef509a2af6787c0e126525e0cd161ca7287602c5
WonGeunKim <kwg4764@naver.com>
2023-05-09 22:05:26 +0900

    - PriorityChange Ex

---
+++ JavaBasic/src/main/java/Thread/PriorityChangeEx.java
@@ -0,0 +1,32 @@
+package Thread;
+
+class SuperThreadPriority extends Thread {
+	
+	public SuperThreadPriority(String threadName) {
+		super(threadName);
+	}
+	
+	public void run() {
+		System.out.printf("스레드 이름 : %s %n" , Thread.currentThread().getName());
+	}
+}
+
+
+public class PriorityChangeEx extends SuperThreadPriority {
+
+	public PriorityChangeEx(String threadName) {
+		super(threadName);
+	}
+
+	
+	public static void main(String[] args) {
+		
+		Thread t1 = new SuperThreadPriority("첫번째 스레드");
+		t1.setPriority(Thread.MIN_PRIORITY);
+		t1.start();
+		
+		Thread t2 = new SuperThreadPriority("두번째 스레드");
+		t2.start();
+	}
+
+}
