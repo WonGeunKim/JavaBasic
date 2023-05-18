adea9991ca7d9b2e4769d95b218195652f70837f
WonGeunKim <kwg4764@naver.com>
2023-05-18 23:09:29 +0900

    - ProducerCustomer Ex

---
+++ JavaBasic/src/main/java/Thread/ProducerCustomerEx.java
@@ -0,0 +1,22 @@
+package Thread;
+
+public class ProducerCustomerEx {
+
+	public static void main(String[] args) {
+		
+		Car c = new Car();
+		
+		Producer producer = new Producer(c);
+		
+		Thread tProducer = new Thread(producer);
+		
+		Customer customer = new Customer(c);
+		
+		Thread tCustomer = new Thread(customer);
+		
+		tProducer.start();
+		tCustomer.start();
+		
+	}
+
+}
